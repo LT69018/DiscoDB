8463e014abd973336cab4902c4d60012524f0790
LT69018 <jturner3@umbc.edu>
2024-03-27 21:00:34 -0400

    Create file for search logic. Continue conversion.
    Todo: mui install. See JIRA
    https://umbc-jturner3.atlassian.net/browse/DIS-54?focusedCommentId=10014
    
    Still in progress moving things from plain html to React.

--- interface/src/App.js
+++ interface/src/App.js
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import "./premade_theme/css/styles.css";
 
 import React from "react";
-import { Button, Navbar, Container } from "react-bootstrap";
+import { Button, Navbar, Container, 
+         Row, Col } 
+        from "react-bootstrap";
+
+import CustomSearchBar from "./Custom/CustomSearchBar.js";
 
 function App() {
 	return (
@@ -22,90 +26,18 @@ function App() {
 			{/* Masthead */}
 			<header className="masthead">
 				<Container style={{ position: "relative" }}>
-					<div className="row justify-content-center">
-						<div className="col-xl-6">
+          <Row style={{ justifyContent: "center" }}>
+            <Col xl={6}>
 							<div className="text-center text-white">
 								{/* Page heading */}
 								<h1 className="mb-5">
 									Generate more leads with a professional landing page!
 								</h1>
-								{/*  Signup form */}
-								{/*  * * * * * * * * * * * * * * * */}
-								{/* * * SB Forms Contact Form * * */}
-								{/* * * * * * * * * * * * * * * * */}
-								{/* This form is pre-integrated with SB Forms. */}
-								{/* To make this form functional, sign up at */}
-								{/* https://startbootstrap.com/solution/contact-forms */}
-								{/* to get an API token! */}
-								<form
-									className="form-subscribe"
-									id="contactForm"
-									data-sb-form-api-token="API_TOKEN"
-								>
-									{/* Email address input */}
-									<div className="row">
-										<div className="col">
-											<input
-												className="form-control form-control-lg"
-												id="emailAddress"
-												type="email"
-												placeholder="Email Address"
-												data-sb-validations="required,email"
-											/>
-											<div
-												className="invalid-feedback text-white"
-												data-sb-feedback="emailAddress:required"
-											>
-												Email Address is required.
-											</div>
-											<div
-												className="invalid-feedback text-white"
-												data-sb-feedback="emailAddress:email"
-											>
-												Email Address Email is not valid.
-											</div>
-										</div>
-										<div className="col-auto">
-											<button
-												className="btn btn-primary btn-lg disabled"
-												id="submitButton"
-												type="submit"
-											>
-												Submit
-											</button>
-										</div>
-									</div>
-									{/* Submit success message */}
-									{/* */}
-									{/* This is what your users will see when the form */}
-									{/* has successfully submitted */}
-									<div className="d-none" id="submitSuccessMessage">
-										<div className="text-center mb-3">
-											<div className="fw-bolder">
-												Form submission successful!
-											</div>
-											<p>To activate this form, sign up at</p>
-											<a
-												className="text-white"
-												href="https://startbootstrap.com/solution/contact-forms"
-											>
-												https://startbootstrap.com/solution/contact-forms
-											</a>
-										</div>
-									</div>
-									{/* Submit error message */}
-									{/* */}
-									{/* This is what your users will see when there is */}
-									{/* an error submitting the form */}
-									<div className="d-none" id="submitErrorMessage">
-										<div className="text-center text-danger mb-3">
-											Error sending message!
-										</div>
-									</div>
-								</form>
+								{/* TODO: insert our custom search bar. */}
+                <CustomSearchBar/>
 							</div>
-						</div>
-					</div>
+            </Col>
+          </Row>
 				</Container>
 			</header>
 			{/* Icons Grid */}

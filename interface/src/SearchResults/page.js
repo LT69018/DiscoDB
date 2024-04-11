98827c81178595b85ae80019d40f9de182735da1
Jessica Turner <jturner3@umbc.edu>
2024-04-11 15:42:55 -0400

    Attempt render test api results (somewhat works)
    Current appearance. Still need to write a column for each thing I want to show.
    ```
    Search Results
    Under Construction :P
    
    No cover available
    No cover available
    Unabled to display row. Got wrong number of items
    Unabled to display row. Got wrong number of items
    ```

--- interface/src/SearchResults/page.js
+++ interface/src/SearchResults/page.js
@@ -1,24 +1,43 @@
 import React from "react";
 
+function renderResultRow(resultRow) {
+    const KEYS_TO_CHECK = ["cover", "artist", "album", "year"];
+    const NUM_ITEMS_PER_ROW = KEYS_TO_CHECK.length;
+
+    if (Object.keys(resultRow).length != NUM_ITEMS_PER_ROW) {
+        console.log("Unable to render this row:", resultRow);
+        return (<div>Unabled to display row. Got wrong number of items</div>);
+    }
+
+    const theCoverImage = resultRow["cover"] !== null ? resultRow["cover"] : <img alt="No cover available"/>
+
+    return (
+        <div class="row">
+            <div class="col">
+                {theCoverImage}
+            </div>
+        </div>
+    );
+}
+
 // not sure why it won't let me use tab size of 2
 function SearchResults() {
     const test_api_result = [
-        {'artist': 'Stevie Wonder', 'album': 'Innervision', 'year': 1970},
-        {'artist': '', 'album': '', 'year': 2000}
+        {'artist': 'Stevie Wonder', 'album': 'Innervision', 'year': 1970, "cover": null},
+        {'artist': '', 'album': '', 'year': 2000, cover: null},
+        {"artist": "Artist one", "album":"Album Name", "cover": "https://picsum.photos/id/237/200/300"},
+       {"artist": "Artist two", "album":"Album Name", "cover": "https://picsum.photos/id/238/200/300"}
     ];
 
-    let resultsToRender = test_api_result.map(row => (
-        <div class="row">
-            <div class="col">
-
-            </div>
-        </div>
+    let renderedResults = test_api_result.map(row => (
+        renderResultRow(row)
     ));
 
     return (
         <div class="container">
             <h1>Search Results</h1>
             <p>Under Construction :P</p>
+            {renderedResults}
         </div>
     );
 }

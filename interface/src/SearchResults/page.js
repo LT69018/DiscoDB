2e769324d791b3736d80f7dc9775ab53096cd5df
Jessica Turner <jturner3@umbc.edu>
2024-04-15 09:41:44 -0400

    Begin filling in null images from dummy call
    The page renders, but none of the images do, it just looks like this rn.
    ```
    Search Results
    Under Construction :P
    
    Invalid coverURL
    Invalid coverURL
    Unabled to display row. Got wrong number of items
    Unabled to display row. Got wrong number of items
    ```

--- interface/src/SearchResults/page.js
+++ interface/src/SearchResults/page.js
@@ -1,20 +1,43 @@
 import React from "react";
 
+const BACKEND_COVER_URL_KEY = "coverURL";
+const BACKEND_ARTIST_NAME_KEY = "artist";
+const BACKEND_ALBUM_NAME_KEY = "album";
+const BACKEND_YEAR_KEY = "year";
+
+const FRONTEND_COVER_IMAGE_KEY = "coverImage";
+
+function populateImagesAndEmptyKeys(searchResult) {
+    if (! (BACKEND_COVER_URL_KEY in searchResult)) {
+        searchResult[BACKEND_COVER_URL_KEY] = <img alt="No cover available"/>;
+    } else if (searchResult[BACKEND_COVER_URL_KEY] == null) {
+        searchResult[FRONTEND_COVER_IMAGE_KEY] = <img alt="No cover available"/>;
+    } else {
+        searchResult[FRONTEND_COVER_IMAGE_KEY] = <img href={searchResult[BACKEND_COVER_URL_KEY]} alt="Invalid coverURL"/>;
+    }
+}
+
 function renderResultRow(resultRow) {
-    const KEYS_TO_CHECK = ["cover", "artist", "album", "year"];
+    const KEYS_TO_CHECK = [
+        BACKEND_COVER_URL_KEY, BACKEND_ARTIST_NAME_KEY, BACKEND_ALBUM_NAME_KEY, BACKEND_YEAR_KEY, // <- from the backend
+        "coverImage" // <- from my helper function
+    ];
     const NUM_ITEMS_PER_ROW = KEYS_TO_CHECK.length;
 
-    if (Object.keys(resultRow).length != NUM_ITEMS_PER_ROW) {
-        console.log("Unable to render this row:", resultRow);
+    const htmlRow = Object.assign({}, resultRow); // fully copy it so we can make changes
+    populateImagesAndEmptyKeys(htmlRow);
+    if (Object.keys(htmlRow).length !== NUM_ITEMS_PER_ROW) {
+        console.log("Unable to render this row:", htmlRow,
+            "\tExpected number of items: ", NUM_ITEMS_PER_ROW, "Got:", Object.keys(htmlRow).length);
         return (<div>Unabled to display row. Got wrong number of items</div>);
     }
 
-    const theCoverImage = resultRow["cover"] !== null ? resultRow["cover"] : <img alt="No cover available"/>
-
+    populateImagesAndEmptyKeys(htmlRow);
+    
     return (
         <div class="row">
             <div class="col">
-                {theCoverImage}
+                {htmlRow["coverImage"]}
             </div>
         </div>
     );
@@ -23,10 +46,10 @@ function renderResultRow(resultRow) {
 // not sure why it won't let me use tab size of 2
 function SearchResults() {
     const test_api_result = [
-        {'artist': 'Stevie Wonder', 'album': 'Innervision', 'year': 1970, "cover": null},
-        {'artist': '', 'album': '', 'year': 2000, cover: null},
-        {"artist": "Artist one", "album":"Album Name", "cover": "https://picsum.photos/id/237/200/300"},
-       {"artist": "Artist two", "album":"Album Name", "cover": "https://picsum.photos/id/238/200/300"}
+        {BACKEND_ARTIST_NAME_KEY: 'Stevie Wonder', BACKEND_ALBUM_NAME_KEY: 'Innervision', BACKEND_YEAR_KEY: 1970, BACKEND_COVER_URL_KEY: null},
+        {BACKEND_ARTIST_NAME_KEY: '', BACKEND_ALBUM_NAME_KEY: '', BACKEND_YEAR_KEY: 2000, BACKEND_COVER_URL_KEY: null},
+        {BACKEND_ARTIST_NAME_KEY: "Artist one", BACKEND_ALBUM_NAME_KEY:"Album Name", BACKEND_COVER_URL_KEY: "https://picsum.photos/id/237/200/300"},
+        {BACKEND_ARTIST_NAME_KEY: "Artist two", BACKEND_ALBUM_NAME_KEY:"Album Name", BACKEND_COVER_URL_KEY: "https://picsum.photos/id/238/200/300"}
     ];
 
     let renderedResults = test_api_result.map(row => (

0179a046e2926d2ba56c30efad0fc86a0c81a5c8
ayushn1001 <anigam2@umbc.edu>
2024-04-17 19:01:30 -0400

    moved default album picture

--- interface/src/AlbumInfo/albumpage.js
+++ interface/src/AlbumInfo/albumpage.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import './styles.css'; // Import the CSS file
 import { Container, Row, Col } from "react-bootstrap";
-
-var albumCoverSrc = "./default_cover_art.jpg"; // Replace with the path to your album cover image
+import album_c from "./default_cover_art.jpg"
+import { useLocation, // get data from previous page
+         // useNavigate // send data to next page
+       } from 'react-router-dom';
 
 export default function AlbumPage(){
+    const location = useLocation();
+    console.log("SearchResults.location=", location);
     return (
         <div className='body'>
-            <div className="row">
-                <div className="album-info">
+            <div className="Row album-info">
+               
                     <div className="column">
                         <div className="album-image">
-                            <img src={albumCoverSrc} alt="Album Cover"/>
+                            <img src={album_c} alt="Album Cover"/>
                         </div>
                     </div>
                     <div className="column">
                         <div className="album-details">
                             <h1>Album Name</h1>
-                            <h2>Artist Name</h2>
+                            <h2>Artist Name</h2> 
+                            {/*<h1>{location.state.album}</h1>
+                            <h2>{location.state.artist}</h2>*/}
                             <p>Description of the album goes here...</p>
                             <h3>Tracklist:</h3>
                             <ul className="tracklist">
@@ -27,7 +33,7 @@ export default function AlbumPage(){
                             </ul>
                         </div>
                     </div>
-                </div>
+                
             </div>
         </div>
     );

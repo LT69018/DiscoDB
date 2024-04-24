d00f11959495702c233820e3ca781bf1568d7ea4
DanWilliams24 <01danielwilliams@gmail.com>
2024-04-24 16:54:29 -0400

    adding boilerplate  express settings

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,23 @@ Description: Express code. Contains endpoints for DiscoDB backend.
 
 const PORT = process.env.PORT;
 
-const express = require("express");
 
-const app = express();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+
+var indexRouter = require("./routes/index");
+// morgan provides easy logging for express, and by default it logs to stdout
+// which is a best practice in Docker. Friends don't let friends code their apps to
+// do app logging to files in containers.
+
+var app = express();
+//manages endpoint routes
+
+
 /* ======================== (start) REFERENCE:github/docker ==================
 /* - these comments and imports come from the reference. */
 // simple node web server that displays hello world
@@ -18,20 +32,39 @@ const app = express();
 // you'll see long build times on every code change + build. If done correctly,
 // code changes should be only a few seconds to build locally due to build cache.
 
-const morgan = require("morgan");
-// morgan provides easy logging for express, and by default it logs to stdout
-// which is a best practice in Docker. Friends don't let friends code their apps to
-// do app logging to files in containers.
-
 const database = require("./database");
 
-app.use(morgan("common"));
 /* ======================== ( end ) REFERENCE:github/docker ================== */
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/testAPI", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
 
 /* ======================== (start) REFERENCE:github/docker ==================
 - these template endpoints are from the reference:  
@@ -39,20 +72,17 @@ app.listen(PORT, () => {
   - "/healthz" - verifying the app is functioning
 */
 
-app.get("/", function(req, res, next) {
-  database.raw('select VERSION() version')
-    .then(([rows, columns]) => rows[0])
-    .then((row) => res.json({ message: `Hello from MySQL ${row.version}` }))
-    .catch(next);
-  res.send("Successfully pinged GET '/'");
-});
 
 app.get("/healthz", function(req, res) {
+
   // do app logic here to determine if app is truly healthy
   // you should return 200 if healthy, and anything else will fail
   // if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
   res.send("I am happy and healthy\n");
 });
+
+
+
 /* ======================== (end) REFERENCE:github/docker ================== */
 
 module.exports = app;

4767261ffba57815563e1686eaee4f00c8fdf183
Jessica Turner <jturner3@umbc.edu>
2024-04-25 11:01:28 -0400

    Add constant for mysql database name.
    -When we make another subfolder to `db` for the larger set, we can change this to just "discodb".
    - Currently me and coby are consolidating our changes so he will merge the changes that use these constants momentarily.

--- db/Miniature database code/config.py
+++ db/Miniature database code/config.py
@@ -1,5 +1,6 @@
 # local paths and file names for raw dataset files
 DATASET_FOLDER_PATH = r"C:\Users\jturn\Documents\UMBC\DiscoDB_Downloads/"
+
 ARTISTS_DATASET_FILE_NAME= "discogs_20240201_artists_modified.xml"
 ALL_RELEASES_DATASET_FILE_NAME = "main_releases_modified.xml"
 
@@ -9,3 +10,5 @@ DISCODB_MYSQL_USERNAME = "root" # <- for docker
 # for local work, can change to yours, i.e. "Coby"
 # todo: in the future, be more secure about the password :0
 DISCODB_MYSQL_PASSWORD = "db-btf5q"
+
+DISCODB_NAME = "discodb_mini"
